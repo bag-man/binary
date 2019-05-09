64047ec2fc567a3c224b8fa5c31c86930463ba4c
Owen Garland <garland.owen@gmail.com>
2019-05-09 17:26:01 +0200

    feat: add assets

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import * as bitcoin from 'bitcoinjs-lib'
 import axios from 'axios'
+import * as fs from 'fs'
+import * as path from 'path'
+import { spawnSync } from 'child_process'
 
-const main = async (): Promise<void> => {
-
+const btc = async (): Promise<void> => {
   const keyPair = bitcoin.ECPair.makeRandom()
   const address = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey }).address
   const secret = keyPair.toWIF()
@@ -19,11 +21,22 @@ const main = async (): Promise<void> => {
   exit()
 }
 
+const readFile = async (): Promise<void> => {
+  const content = fs.readFileSync(path.join(__dirname, 'assets/data.json'), 'utf8')
+  console.log(JSON.parse(content))
+}
+
 const exit = () => {
   process.stdout.write('\nPress any key to exit.\n')
-  process.stdin.setRawMode(true)
+  process.stdin.setRawMode!(true)
   process.stdin.resume()
   process.stdin.on('data', process.exit.bind(process, 0))
+}
+
+const main = async () => {
+  await readFile()
+  await btc()
+  exit()
 }
 
 main()
